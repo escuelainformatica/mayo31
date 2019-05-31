decc7f532d17e09bb1ffacdb2a4bde97e8bd458d
Jorge Castro <jorge.castro.c@gmail.com>
2019-05-31 19:43:12 -0400

    primera version

---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp11.modelo;
+using WindowsFormsApp11.servicio;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // crear dos objetos.
+            int variable=20;
+            string texto="aaa";
+            Compra compra1=new Compra();
+            Compra compra2 = new Compra();
+            compra1=CompraServicio.Factory(textBox1,textBox2);
+            compra2 = CompraServicio.Factory(textBox5, textBox4);
+
+            double total=CompraServicio.Sumar(compra1,compra2);
+
+            textBox3.Text=total.ToString();
+
+
+        }
+    }
+}
---
+++ WindowsFormsApp11/servicio/CompraServicio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp11.modelo;
+
+namespace WindowsFormsApp11.servicio
+{
+    class CompraServicio
+    {
+        public static double Sumar(Compra compra1,Compra compra2)
+        {
+            double r=0;
+            r=compra1.Precio+compra2.Precio;
+            //r=compra1+compra2;
+
+            return r;
+        }
+        public static Compra Factory(TextBox texto1,TextBox texto2)
+        {
+            Compra compra=new Compra(); 
+            compra.Nombre=texto1.Text;
+            compra.Precio=Convert.ToDouble(texto2.Text);
+            return compra;
+        }
+    }
+}
